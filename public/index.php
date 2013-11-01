e02e1fb330f36232b1caab1e01bd56812c184617
Leonardo Rocha <leonardo.lsrocha@gmail.com>
2013-11-01 11:37:37 -0200

    User model: add()

--- public/index.php
+++ public/index.php
@@ -6,5 +6,16 @@ require ROOT.DS.'vendor'.DS.'autoload.php';
 use EACHeRepublica\Model\User;
 
 $test = new User();
+
+$data = array(
+    'name' => 'Leonardo Santos Rocha',
+    'email' => 'leonardo@outlook.com.br',
+    'password' => 'blablabla1',
+    'salt' => 'saltinbancos',
+    'novo' => 'algumcoisa'
+);
+
+var_dump($test->add($data));
+
 // var_dump($test->delete(18));
 // print_r($test->listAll());
--- src/EACHeRepublica/Model/User.php
+++ src/EACHeRepublica/Model/User.php
@@ -9,8 +9,27 @@ use EACHeRepublica\Lib\Model;
  */
 class User extends Model
 {
-    public function add()
+    public function add(array $values)
     {
+        $keys = array('name', 'password', 'email', 'salt');
+        $diff = array_diff($keys, array_keys($values));
+
+        // Checking if array has the required keys
+        if (!empty($diff)) {
+            return false;
+        }
+
+        $query = $this->database->prepare(
+            'INSERT INTO users (name, email, password, salt)'.
+            'VALUES (:name, :email, :password, :salt)'
+        );
+
+        $query->bindParam(':name', $values['name'], PDO::PARAM_STR);
+        $query->bindParam(':email', $values['email'], PDO::PARAM_STR);
+        $query->bindParam(':password', $values['password'], PDO::PARAM_STR);
+        $query->bindParam(':salt', $values['salt'], PDO::PARAM_STR);
+
+        return $query->execute();
     }
 
     public function delete($id)
