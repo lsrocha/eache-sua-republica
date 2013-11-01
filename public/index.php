ca1dde7cf1b1e32bf91091740b2619b9f9002e33
Leonardo Rocha <leonardo.lsrocha@gmail.com>
2013-10-31 22:17:44 -0200

    Model User: delete() e exists()

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@ require '../src/EACHeRepublica/Model/User.php';
 use EACHeRepublica\Model\User;
 
 $test = new User();
-print_r($test->listAll());
+// var_dump($test->delete(18));
+// print_r($test->listAll());
--- src/EACHeRepublica/Model/User.php
+++ src/EACHeRepublica/Model/User.php
@@ -3,8 +3,8 @@ namespace EACHeRepublica\Model;
 
 require '/srv/www/EACHe-sua-republica/src/EACHeRepublica/Lib/Model.php';
 
-use EACHeRepublica\Lib\Model;
 use \PDO;
+use EACHeRepublica\Lib\Model;
 
 /**
  * @author Leonardo Rocha <leonardo.lsrocha@gmail.com>
@@ -15,9 +15,26 @@ class User extends Model
     {
     }
 
+    public function delete($id)
+    {
+        $query = $this->database->prepare('DELETE FROM users WHERE id = :id');
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        return ($query->rowCount() === 1);
+    }
+
     public function listAll()
     {
         $query = $this->database->query('SELECT name, email FROM users');
         return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function exists($email)
+    {
+        $query = $this->database->prepare(
+            'SELECT * FROM users WHERE email = :email'
+        );
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->execute();
     }
 }
